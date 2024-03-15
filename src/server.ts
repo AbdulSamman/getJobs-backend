72990aed1f2bb1610825ec335314d0442f682377
AbdulSamman <sammanab@outlook.de>
2024-03-15 01:40:29 +0100

    express server works

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,20 @@
 import express from "express"
+import fs from "fs";
 
+const app=express()
+const port = 3668
+
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json","utf8"))
+
+app.get("/",(req:express.Request,res:express.Response)=>{
+res.send("Jobs site API")
+})
+
+app.get("/jobs",(req:express.Request,res:express.Response)=>{
+    res.json((jobs))
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+
+})
